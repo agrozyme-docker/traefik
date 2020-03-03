3e7bec550815e1fa56b8c018438018e36586db8f
agrozyme <agrozyme@gmail.com>
2020-03-03 17:48:43 +0800

    update

--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -1,17 +1,23 @@
 #!/usr/bin/lua
 local core = require("docker-core")
 
+local function update_settings(items)
+  local text = string.gsub([==[
+
+[file]
+  directory = "$etc"
+  watch = true
+
+    ]==], "$(%w+)", items)
+
+  core.append_file("/etc/traefik/traefik.toml", text)
+end
+
 local function main()
   local etc = "/usr/local/etc/traefik"
   core.run("apk add --no-cache traefik@testing")
   core.run("mkdir -p %s", etc)
-  core.append_file(
-    "/etc/traefik/traefik.toml",
-    "\n",
-    "[file] \n",
-    string.format('  directory = "%s" \n', etc),
-    "  watch = true \n"
-  )
+  update_settings({etc = etc})
 end
 
 main()
